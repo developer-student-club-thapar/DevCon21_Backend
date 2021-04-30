316897d9fc3ee736d867c0b3a173069d78ba49f3
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-04-30 10:40:32 +0530

    Django and development setup: 	- Initialize django project 	- django-environ to hide app secrets 	- modularize settings for dev and deployment on heroku 	- pre-commit config

---
+++ app/settings/prod.py
@@ -0,0 +1,19 @@
+import os
+import dj_database_url
+
+from app.settings.base import *  # noqa
+
+
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq"
+)
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+ALLOWED_HOSTS = []
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
+# database config
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)  # noqa
