ddcabcbcc4f18b50b373f7698df581d23600affa
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-04-30 12:38:18 +0530

    getting ready for deployment

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY", "p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq"
 )
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["devcon21.herokuapp.com"]
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
