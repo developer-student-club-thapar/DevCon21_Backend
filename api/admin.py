3337c4e019d2396c04c9bfd2df4a1a430e2387c7
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-04-30 12:57:39 +0530

    Import export config

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from api.models import Participant
 
 
-class ParticipantAdmin(admin.ModelAdmin):
+class ParticipantResource(resources.ModelResource):
+    class Meta:
+        model = Participant
+
+
+class ParticipantAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     model = Participant
     list_display = ["__str__", "email", "institution", "mobile"]
     list_filter = ["institution", "yos_designation", "referralCode"]
